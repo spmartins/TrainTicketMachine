5bd03d09fec7fc4ce26eba773328faaa51053b42
Altran <Altran@EXET35MKE1407.ptportugal.corppt.com>
2018-10-10 18:25:45 +0100

    Add project files.

---
+++ TrainTicketMachine/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrainTicketMachine.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
